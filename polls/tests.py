038644b243b446cb88924e4c20006f12f139d002
Misael <misaelnodaquintero@gmail.com>
2017-12-09 13:25:55 +0100

    test

--- polls/tests.py
+++ polls/tests.py
@@ -14,7 +14,7 @@ def create_question(question_text, days):
     in the past, positive for questions that have yet to be published).
     """
     time = timezone.now() + datetime.timedelta(days=days)
-    return Question.objects.create(question_text=question_text, pub_date=time)
+    return Question.objects.create(question_text=question_text )
 
 
 class QuestionViewTests(TestCase):
